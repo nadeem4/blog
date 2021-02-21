ad6846bb987c129bcf55675c7eb8755393b586e2
nadeem <nadeem4.nk13@gmail.com>
2021-02-21 08:54:35 -0500

    added pages and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostPageComponent } from './pages/post-page/post-page.component';
+import { BlogsPageComponent } from './pages/blogs-page/blogs-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutUsPageComponent } from './pages/about-us-page/about-us-page.component';
+import { ContactUsPageComponent } from './pages/contact-us-page/contact-us-page.component';
+import { PrivacyPolicyPageComponent } from './pages/privacy-policy-page/privacy-policy-page.component';
+import { TermsAndConditionPageComponent } from './pages/terms-and-condition-page/terms-and-condition-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostPageComponent,
+    BlogsPageComponent,
+    HomePageComponent,
+    AboutUsPageComponent,
+    ContactUsPageComponent,
+    PrivacyPolicyPageComponent,
+    TermsAndConditionPageComponent,
   ],
   imports: [
     BrowserModule,
